4d734e088d0a4e17b3b8b7791b56edbbcf3c3881
DanVah <dansvahrovs@gmail.com>
2020-10-20 11:42:31 +0300

    Java day 2 11:45

---
+++ SEF/src/sef/module2/sample/HelloWorld1.java
@@ -0,0 +1,9 @@
+package sef.module2.sample;
+
+public class HelloWorld1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World! - Dan Vahrov");
+	}
+}
--- SEF/src/sef/module3/activity/VariableAssignmentActivity.java
+++ SEF/src/sef/module3/activity/VariableAssignmentActivity.java
@@ -12,7 +12,13 @@ public class VariableAssignmentActivity {
 	/**
 	 * @param args
 	 */
+	public static int myNumber = 1;
+	
 	public static void main(String[] args) {
+		
+		myNumber = 2;
+		
+		System.out.println("my number: "+myNumber);
 		
 		// 1- Declare a variable of type int and assign it default value.
 		// 2- Update the value 
